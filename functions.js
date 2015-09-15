e1a0676f3b55e41a6736c3a42670fa6cafaf37cc
Mike Finley <mfinley333@users.noreply.github.com>
2015-09-15 12:49:50 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,9 +3,13 @@ var showCards = document.getElementById("cards");
 function newDeck(){
 
   var ranks = [ {card:"1", points: 1 },
-               { card: "2", points: 2 }, { card: "3", points: 3 }, { card: "4", points: 4 }, { card: "5", points: 5 }, { card: "6", points: 6 }];
+               //{ card: "2", points: 2 },
+               //{ card: "3", points: 3 },
+               { card: "7", points: 7 },
+               { card: "5", points: 5 },
+               { card: "6", points: 6 }];
   
-  var suits = [ "U"];
+  var suits = [ "K"];
   var numOfCards = ranks.length*suits.length;
   
   var deck = [];
@@ -24,8 +28,12 @@ function newDeck(){
 function displayCards(){
   var deck = newDeck();
   var shuffledCards = shuffleCards(deck);
-  var imageURL = [{ chosenCard: "U-1", imgURL: "http://static.wixstatic.com/media/afc4a4_7bf6fddf12664eb1a04eebf3dd9440b3.jpg_srz_p_596_431_75_22_0.50_1.20_0.00_jpg_srz" },
-             { card: "2", points: 2 }, { card: "3", points: 3 }, { card: "4", points: 4 }, { card: "5", points: 5 }, { card: "6", points: 6 }];
+  var imageURL = [{ chosenCard: "1", imgURL: "http://static.wixstatic.com/media/afc4a4_7bf6fddf12664eb1a04eebf3dd9440b3.jpg_srz_p_596_431_75_22_0.50_1.20_0.00_jpg_srz" },
+             //{ chosenCard: "2", imgURL: 2 },
+             //{ chosenCard: "3", imgURL: 3 },
+             { chosenCard: "7", imgURL: "http://static.wixstatic.com/media/afc4a4_49f507a796334fd5b9c1fcc562c88acb.jpg_srz_p_231_378_75_22_0.50_1.20_0.00_jpg_srz" },
+             { chosenCard: "5", imgURL: "http://static.wixstatic.com/media/afc4a4_24691f7fd30e4741997cbe1a93df0e9d.jpg_srz_p_247_393_75_22_0.50_1.20_0.00_jpg_srz" },
+             { chosenCard: "6", imgURL: "http://static.wixstatic.com/media/afc4a4_3e3f7275aa1e4fa893ff744f5acb6a05.jpg_srz_p_209_356_75_22_0.50_1.20_0.00_jpg_srz" }];
 
   
   for(var i=0; i < 1; i++){
@@ -33,8 +41,16 @@ function displayCards(){
     card.className = "card";
     var cardContainer = document.getElementById('container');
     cardContainer.appendChild(card);
-    //URL NOT WORKING
-    card.style.backgroundImage = "url(card-img/" + shuffledCards[i].suit + "-" + shuffledCards[i].card + ".jpg" + ")";
+      //URL NOT WORKING
+    var imgPathURL;
+    for (var j = 0; j < imageURL.length; j++) {
+        if (imageURL[j].chosenCard == shuffledCards[i].card) {
+            imgPathURL = imageURL[j].imgURL;
+            break;
+        }
+    }
+    card.style.backgroundImage = "url(" + imgPathURL + ")";
+    card.style.backgroundRepeat = false;
   }
 }
 
